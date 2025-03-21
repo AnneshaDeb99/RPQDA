d2a254cef283283f88b24f6d2b382dfe86b44c21
AnneshaDeb99 <123144858+AnneshaDeb99@users.noreply.github.com>
2025-03-21 15:24:48 +0530

    Update Main_Parallel.R

--- Main_Parallel.R
+++ Main_Parallel.R
@@ -37,19 +37,18 @@ method <- 'all'
 
 p_all <- c(2^(8:13),10000)   #all the dimensions to be evaluated
 
-p_all = 100
 
-n1 <- 100 #train sample size population 1
-n2 <- 100 #train sample size population 2
-m1 <- 200 #test sample size population 1
-m2 <- 200 #test sample size population 2
+n1 <- 100 #train sample size class 1
+n2 <- 100 #train sample size class 2
+m1 <- 200 #test sample size class 1
+m2 <- 200 #test sample size class 2
 
 iter <- 10
 
 run <- function(i){
   file_name <- paste0(scheme[i], 'of', method, '.RData')
   cat("Starting model", i, "...\n")
-  result <- Experiment(scheme = scheme[i],method = method, p_all = p_all,iter = iter,n1 = n1,n2 = n2, m1 = m1, m2 = m2, file_name = file_name)
+  result <- Experiment(scheme = scheme[i],method = method, p_all = p_all,iter = iter, n1 = n1, n2 = n2, m1 = m1, m2 = m2, file_name = file_name)
   MP <- result$MP
   Ti <- result$Ti
   save(MP,Ti, file = file_name)
