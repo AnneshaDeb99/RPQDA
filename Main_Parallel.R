8f3eb132bbaf39b4d4f640604fed55460698f858
AnneshaDeb99 <123144858+AnneshaDeb99@users.noreply.github.com>
2025-03-21 15:22:34 +0530

    Add files via upload

---
+++ Main_Parallel.R
@@ -0,0 +1,64 @@
+################################################################################
+################################################################################
+
+# 1. Model.R will give us population parameters mu1, mu2, sigma1 and sigma2
+#
+# 2. Given the parameters generateData_Bayes.R will generate data (X1) and (X2)
+#    and calculates class assignments using bayes rule (res)
+#
+# 3. Using the generated data, Experiment.R will calculate MP (Misclassification
+#    proportion) and Ti (Time) for each methods (e.g. Bayes, RPE-SN etc.), for
+#    each dimensions and iterations.
+#
+################################################################################
+################################################################################
+
+rm(list = ls())
+path <- "C:/Users/Annsha deb/Dropbox/Random_Projection/QDA/QDA_Codes/Codes(Submitted)-Final"
+
+#path <- readline("Enter the path to the Codes folder: ")
+setwd(path)
+
+library(MASS)
+library(parallel)
+library(doSNOW)
+library(doParallel)
+  
+
+source('Experiment.R')
+
+cl <- makeCluster(8, type = "SOCK")
+registerDoSNOW(cl)
+
+scheme <- c('scheme1','scheme2','scheme3','scheme4')  #Specifying the model, types of model are specified in Model.R file
+
+
+method <- 'all'
+
+p_all <- c(2^(8:13),10000)   #all the dimensions to be evaluated
+
+p_all = 100
+
+n1 <- 100 #train sample size population 1
+n2 <- 100 #train sample size population 2
+m1 <- 200 #test sample size population 1
+m2 <- 200 #test sample size population 2
+
+iter <- 10
+
+run <- function(i){
+  file_name <- paste0(scheme[i], 'of', method, '.RData')
+  cat("Starting model", i, "...\n")
+  result <- Experiment(scheme = scheme[i],method = method, p_all = p_all,iter = iter,n1 = n1,n2 = n2, m1 = m1, m2 = m2, file_name = file_name)
+  MP <- result$MP
+  Ti <- result$Ti
+  save(MP,Ti, file = file_name)
+}
+
+
+print(date())
+foreach(i = 1:length(scheme), .packages = 'doParallel')%dopar%{run(i)}
+print(date())
+
+
+
---
+++ RPE.R
@@ -0,0 +1,172 @@
+################################################################################
+################################# RPE.R ########################################
+################################################################################
+
+# Inputs: type: distribution of random matrix to be used (e.g., 'StdNormal', '3_point' etc)
+#         X_train: n x p dimensional data matrix (covariates of training observations)
+#         y_train: classes corresponding to the training data
+#         X_test: m x p dimensional data matrix (covariates of test observations)
+#         d: reduced dimension
+#         B: number of random matrices to be used
+#
+# Outputs: class.RPE: estimated classes 
+
+################################################################################
+################################################################################
+################################################################################
+
+library(abind)
+
+RPE <- function(type, X_train, y_train, X_test, d = 10, B = 500){
+  
+  if(is.vector(X_test) == TRUE){
+    X_test = t(as.matrix(X_test))
+  }
+  k <- length(unique(y_train)) #number of classes 
+  p <- ncol(X_test)
+  n.test <- nrow(X_test)
+  #y_train = ifelse(y_train == 0, 1, 2)
+  
+  if(type == 'StdNormal'){
+    q_RP_hat_ens <- foreach(b = 1:B, .combine = function(...) abind(..., along = 3), .multicombine = TRUE, .init = NULL, .packages = c('doParallel','abind','Matrix')) %dopar% {
+      
+      set.seed(2024 + b)
+      R <- matrix(rnorm(d*p,0,1), nrow = p, ncol = d)
+      XRP_train <- crossprod(t(X_train), R)
+      XRP_test <- crossprod(t(X_test), R)
+      n.train <- nrow(XRP_train)
+      n.test <- nrow(XRP_test)
+      q.RP.hat = matrix(0, ncol = k, nrow = n.test)
+      
+      for(i in 1:n.test){
+        
+        xrp.te = XRP_test[i, ]
+        q.RP.hat[i, ] = sapply(1:k, function(j){
+          n = sum(y_train == j);
+          class = which(y_train == j);
+          xrp.train = XRP_train[class, ];
+          mu = colMeans(xrp.train);
+          sig = crossprod(xrp.train - mu, xrp.train - mu)/(n-1);
+          dis = - (0.5*log(abs(det(sig)))) - (0.5*tcrossprod((tcrossprod(xrp.te - mu, solve(sig))), xrp.te - mu)) + (log(n/n.train));
+          return(dis)})
+      }
+      return(q.RP.hat)
+    }
+  } 
+  else if(type == 'Normal'){
+    q_RP_hat_ens <- foreach(b = 1:B, .combine = function(...) abind(..., along = 3), .multicombine = TRUE, .init = NULL, .packages = c('doParallel','abind')) %dopar% {
+      
+      set.seed(2024 + b)
+      R <- matrix(rnorm(d*p,0,(1/d)),nrow=p,ncol=d)
+      XRP_train <- crossprod(t(X_train), R)
+      XRP_test <- crossprod(t(X_test), R)
+      n.train <- nrow(XRP_train)
+      n.test <- nrow(XRP_test)
+      q.RP.hat = matrix(0, ncol = k, nrow = n.test)
+      
+      for(i in 1:n.test){
+        
+        xrp.te = XRP_test[i, ]
+        q.RP.hat[i, ] = sapply(1:k, function(j){
+          n = sum(y_train == j);
+          class = which(y_train == j);
+          xrp.train = XRP_train[class, ];
+          mu = colMeans(xrp.train);
+          sig = crossprod(xrp.train - mu, xrp.train - mu)/(n-1);
+          dis = - (0.5*log(det(sig))) - (0.5*tcrossprod((tcrossprod(xrp.te - mu, solve(sig))), xrp.te - mu)) + (log(n/n.train));
+          return(dis)})
+      }
+      return(q.RP.hat)
+    }
+  }else if(type == 'Std+1/-1'){
+    q_RP_hat_ens <- foreach(b = 1:B, .combine = function(...) abind(..., along = 3), .multicombine = TRUE, .init = NULL, .packages = c('doParallel','abind')) %dopar% {
+      
+      set.seed(2024 + b)
+      r <- rbinom(d*p, 1, 0.5)
+      r[r == 0] <- -1
+      R <- matrix(r/d , nrow = p, ncol = d)
+      
+      XRP_train <- crossprod(t(X_train), R)
+      XRP_test <- crossprod(t(X_test), R)
+      n.train <- nrow(XRP_train)
+      n.test <- nrow(XRP_test)
+      q.RP.hat = matrix(0, ncol = k, nrow = n.test)
+      
+      for(i in 1:n.test){
+        
+        xrp.te = XRP_test[i, ]
+        q.RP.hat[i, ] = sapply(1:k, function(j){
+          n = sum(y_train == j);
+          class = which(y_train == j);
+          xrp.train = XRP_train[class, ];
+          mu = colMeans(xrp.train);
+          sig = crossprod(xrp.train - mu, xrp.train - mu)/(n-1);
+          dis = - (0.5*log(det(sig))) - (0.5*tcrossprod((tcrossprod(xrp.te - mu, solve(sig))), xrp.te - mu)) + (log(n/n.train));
+          return(dis)})
+      }
+      return(q.RP.hat)
+    }
+  }
+  else if(type == '+1/-1'){
+    q_RP_hat_ens <- foreach(b = 1:B, .combine = function(...) abind(..., along = 3), .multicombine = TRUE, .init = NULL, .packages = c('doParallel','abind')) %dopar% {
+      
+      set.seed(2024 + b)
+      r <- rbinom(d*p, 1, 0.5)
+      r[r == 0] <- -1
+      R <- matrix(r, nrow = p, ncol = d)
+      
+      XRP_train <- crossprod(t(X_train), R)
+      XRP_test <- crossprod(t(X_test), R)
+      n.train <- nrow(XRP_train)
+      n.test <- nrow(XRP_test)
+      q.RP.hat = matrix(0, ncol = k, nrow = n.test)
+      
+      for(i in 1:n.test){
+        
+        xrp.te = XRP_test[i, ]
+        q.RP.hat[i, ] = sapply(1:k, function(j){
+          n = sum(y_train == j);
+          class = which(y_train == j);
+          xrp.train = XRP_train[class, ];
+          mu = colMeans(xrp.train);
+          sig = crossprod(xrp.train - mu, xrp.train - mu)/(n-1);
+          dis = - (0.5*log(det(sig))) - (0.5*tcrossprod((tcrossprod(xrp.te - mu, solve(sig))), xrp.te - mu)) + (log(n/n.train));
+          return(dis)})
+      }
+      return(q.RP.hat)
+    }
+  }  
+  else if(type == '3_point'){
+    q_RP_hat_ens <- foreach(b = 1:B, .combine = function(...) abind(..., along = 3), .multicombine = TRUE, .init = NULL, .packages = c('doParallel','abind')) %dopar% {
+      
+      set.seed(2024 + b)
+      R <- matrix(sample(c(1,0,-1),(d*p),replace = TRUE, prob = c(1/(2*sqrt(p)), 1 - 1/sqrt(p), 1/(2*sqrt(p)))), nrow = p, ncol = d)
+      
+      XRP_train <- as.matrix(crossprod(t(X_train), R))
+      XRP_test <- as.matrix(crossprod(t(X_test), R))
+      n.train <- nrow(XRP_train)
+      n.test <- nrow(XRP_test)
+      q.RP.hat = matrix(0, ncol = k, nrow = n.test)
+      
+      for(i in 1:n.test){
+        
+        xrp.te = XRP_test[i, ]
+        q.RP.hat[i, ] = sapply(1:k, function(j){
+          n = sum(y_train == j);
+          class = which(y_train == j);
+          xrp.train = XRP_train[class, ];
+          mu = colMeans(xrp.train);
+          sig = crossprod(xrp.train - mu, xrp.train - mu)/(n-1);
+          dis = - (0.5*log(abs(det(sig)))) - (0.5*tcrossprod((tcrossprod(xrp.te - mu, solve(sig))), xrp.te - mu)) + (log(n/n.train));
+          return(dis)})
+      }
+      return(q.RP.hat)
+    }
+  }
+  class.RPE = sapply(1:n.test, function(n){
+    discriminant = rowMeans(q_RP_hat_ens[n,,]);
+    class = which(discriminant == max(discriminant))
+    return(class)
+  })
+  return(class.RPE)
+}
